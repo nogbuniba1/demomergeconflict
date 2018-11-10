b297c3bb1206324e556a39aa6a4741fe425a1648
nogbuniba1 <39070816+nogbuniba1@users.noreply.github.com>
2018-11-09 19:34:14 -0700

    Create console app and update ReadMe

---
+++ MergeConflictSol/MergeConflict/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MergeConflict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
